4f429426eba03209a2f27f71d526dd6387504031
Akshay Rajan <akshayrajan345@gmail.com>
2024-02-17 19:44:27 +0530

    Calculate Target Time

--- background.js
+++ background.js
@@ -3,7 +3,9 @@ let targetTime;
 let isRunning = false;
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+
     if (message.action === "startTimer") {
+
         if (!isRunning) {
             isRunning = true;
             targetTime = message.targetTime;
@@ -17,24 +19,28 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
                 }
             }, 1000);
         }
+
     } else if (message.action === "stopTimer") {
+
         clearInterval(timerInterval);
         timerInterval = undefined;
         isRunning = false;
         updateTimer();
+
     } else if (message.action === "resetTimer") {
+
         clearInterval(timerInterval);
         timerInterval = undefined;
         isRunning = false;
         targetTime = 0;
         updateTimer();
+        
     }
 });
 
 function updateTimer() {
     const remainingTime = Math.max(targetTime - Math.floor((Date.now() / 1000)), 0);
     // Handle updating the timer display in the popup if needed
-    // You can send a message back to the popup to update the display
     // chrome.runtime.sendMessage({ action: "updateTimerDisplay", remainingTime });
     if (remainingTime === 0) {
         if (isRunning) {
--- popup.js
+++ popup.js
@@ -1,23 +1,33 @@
-// Sending messages to background.js to control the timer
-function startTimer() {
-    const duration = timer.value;
-    if (!isRunning) {
-        isRunning = true;
-        const [hr, min, sec] = duration.split(": ").map(Number);
-        targetTime = Math.floor((Date.now() / 1000)) + timeToSeconds(hr, min, sec);
-        timerInterval = setInterval(function () {
-            updateTimer();
-            if (targetTime <= Math.floor((Date.now() / 1000))) {
-                clearInterval(timerInterval);
-                timerInterval = undefined;
-                isRunning = false;
-                updateTimer();
-            }
-        }, 1000);
+document.getElementById("start").addEventListener("click", startTimer);
+document.getElementById("pause").addEventListener("click", stopTimer);
+document.getElementById("reset").addEventListener("click", resetTimer);
+    
 
-        // Send a message to background.js to start the timer
-        chrome.runtime.sendMessage({ action: "startTimer", targetTime });
-    }
+
+// * Start the Timer
+function startTimer() {
+
+    let timer = document.getElementById("timer");
+    const duration = timer.value;
+    const [hr, min, sec] = duration.split(": ").map(Number);
+    let targetTime = Math.floor((Date.now() / 1000)) + timeToSeconds(hr, min, sec);
+    
+    // ! ------------------- Log -------------------
+    console.log(targetTime);
+    
+
+    // let timerInterval = setInterval(function () {
+    //     // updateTimer();
+    //     if (targetTime <= Math.floor((Date.now() / 1000))) {
+    //         clearInterval(timerInterval);
+    //         timerInterval = undefined;
+    //         // updateTimer();
+    //     }
+    // }, 1000);
+
+    // Send a message to background.js to start the timer
+    chrome.runtime.sendMessage({ action: "startTimer", targetTime });
+
 }
 
 function stopTimer() {
@@ -40,4 +50,11 @@ function resetTimer() {
 
     // Send a message to background.js to reset the timer
     chrome.runtime.sendMessage({ action: "resetTimer" });
+}
+
+
+// ? --- Helper functions ---
+
+function timeToSeconds(hours, minutes, seconds) {
+    return hours * 3600 + minutes * 60 + seconds;
 }
