4017dc8c4b4e2d62d3b359eea42cbf6f7e18d259
Akshay Rajan <akshayrajan345@gmail.com>
2024-02-26 15:06:15 +0530

    All Set

--- popup.js
+++ popup.js
@@ -63,14 +63,14 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
 
 // * Functions to convert time in the format "hh: mm: ss" to seconds and back
 function timeToSeconds(duration) {
-    const [hr, min, sec] = duration.split(": ").map(Number);
+    const [hr, min, sec] = duration.split(":").map(Number);
     return hr * 3600 + min * 60 + sec;
 }
 function secondsToTime(seconds) {
     const hr = Math.floor(seconds / 3600).toString().padStart(2, "0");
     const min = Math.floor((seconds % 3600) / 60).toString().padStart(2, "0");
     const sec = (seconds % 60).toString().padStart(2, "0");
-    return `${hr}: ${min}: ${sec}`;
+    return `${hr}:${min}:${sec}`;
 }
 
 // * Clear the timer
@@ -80,7 +80,23 @@ function clearTimer() {
 
 // * Check whether the timer input is of the right format
 function inputVerify(duration) {
-    const regex = /^(0[0-9]|1[0-2]): ([0-5][0-9]): ([0-5][0-9])$/;
+    const regex = /^(0[0-9]|1[0-2]):([0-5][0-9]):([0-5][0-9])$/;
     return regex.test(duration);
 }
 
+// * Add ":" to input field
+document.getElementById('timer').addEventListener('input', (event) => {
+    let value = event.target.value;
+    
+    if (event.inputType === 'deleteContentBackward' || event.inputType === 'deleteContentForward') {
+        // Handle delete key press
+        if (value.length === 3 || value.length === 6) {
+            event.target.value = value.substring(0, value.length - 1);
+        }
+    } else {
+        // Handle other input
+        if (value.length === 2 || value.length === 5) {
+            event.target.value += ':';
+        }
+    }
+});
